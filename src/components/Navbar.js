40bda5d6c3bb26b4eb5894412b89ff19edfecfc1
Belong112 <belongbelong223@gmail.com>
2019-07-30 03:24:09 +0800

    'ver1.5'

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import horse from "../img/horse.jpg";
-import logo from "../img/ntu.png";
+import logo from "../img/ntu.png"
+import {Link} from "react-router-dom";
 
 class Navbar extends Component {
   render() {
@@ -12,33 +13,33 @@ class Navbar extends Component {
             <h4 style={{fontFamily:"Arial"}}>National Taiwan University</h4>          
         </div>
         <div className="navbar-container">
-            <nav id="navbar" class="navbar navbar-expand-xl navbar-dark" style={{transition: ".5s"}}>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample06" aria-controls="navbarsExample06" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
+            <nav id="navbar" className="navbar navbar-expand-xl navbar-dark" style={{transition: ".5s"}}>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample06" aria-controls="navbarsExample06" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse navbar-items" id="navbarsExample06">
-                    <ul class="navbar-nav mr-auto">
-                        <li class="nav-item active">
-                            <a class="nav-link" href="./">首頁</a>
+                <div className="collapse navbar-collapse navbar-items" id="navbarsExample06">
+                    <ul className="navbar-nav mr-auto">
+                        <li className="nav-item active">
+                            <Link className="nav-link" to="./">首頁</Link>
                         </li>
-                        <li class="nav-item dropdown active">
-                          <a class="nav-link dropdown-toggle" href="./research" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">關於我們</a>
-                          <div class="dropdown-menu">
-                            <a class="dropdown-item" href="./research">研究計畫</a>
-                            <a class="dropdown-item" href="./advisor">指導教授</a>
+                        <li className="nav-item dropdown active">
+                          <a className="nav-link dropdown-toggle" href="./research" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">關於我們</a>
+                          <div className="dropdown-menu">
+                            <a className="dropdown-item" href="./research">研究計畫</a>
+                            <a className="dropdown-item" href="./advisor">指導教授</a>
                           </div>
                         </li>
-                        <li class="nav-item active"> 
-                          <a class="nav-link" href="./news">最新消息</a>
+                        <li className="nav-item active"> 
+                          <Link className="nav-link" href="./news">最新消息</Link>
                         </li>
-                        <li class="nav-item active">
-                            <a class="nav-link" href="./member">實驗室成員</a>
+                        <li className="nav-item active">
+                            <a className="nav-link" href="./member">實驗室成員</a>
                         </li>
-                        <li class="nav-item active">
-                            <a class="nav-link" href="./project">課程</a>
+                        <li className="nav-item active">
+                            <a className="nav-link" href="./project">課程</a>
                         </li>
-                        <li class="nav-item active">
-                            <a class="nav-link" href="./contact">聯絡我們</a>
+                        <li className="nav-item active">
+                            <a className="nav-link" href="./contact">聯絡我們</a>
                         </li>                         
                     </ul>
                 </div>
