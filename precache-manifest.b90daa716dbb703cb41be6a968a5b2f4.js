1ec64fc497342869365142636992e044bafef5c5
belong112 <b05901159@ntu.edu.tw>
2019-08-02 17:11:27 +0800

    Updates

---
+++ precache-manifest.b90daa716dbb703cb41be6a968a5b2f4.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09abb2b04114a7335cf97a3a5f14291a",
+    "url": "/iot-lab/index.html"
+  },
+  {
+    "revision": "377bb58526cd2924b2ae",
+    "url": "/iot-lab/static/css/2.8294e1b9.chunk.css"
+  },
+  {
+    "revision": "be75f3354e918cfdf2c2",
+    "url": "/iot-lab/static/css/main.9471cb93.chunk.css"
+  },
+  {
+    "revision": "377bb58526cd2924b2ae",
+    "url": "/iot-lab/static/js/2.77cb2f34.chunk.js"
+  },
+  {
+    "revision": "be75f3354e918cfdf2c2",
+    "url": "/iot-lab/static/js/main.d04ed89a.chunk.js"
+  },
+  {
+    "revision": "cfd9af4330f965d3dd19",
+    "url": "/iot-lab/static/js/runtime~main.70ebe721.js"
+  },
+  {
+    "revision": "8d530f571c2c48ca38ed0b9a72658589",
+    "url": "/iot-lab/static/media/4.8d530f57.jpg"
+  },
+  {
+    "revision": "9910c84c400307feb4d0ae07c83fa3dd",
+    "url": "/iot-lab/static/media/6.9910c84c.jpg"
+  },
+  {
+    "revision": "7981989da0a014b59acc0ef0643ab571",
+    "url": "/iot-lab/static/media/8.7981989d.jpg"
+  },
+  {
+    "revision": "713d1a799dfeaf33e9a1c1b698abd8b6",
+    "url": "/iot-lab/static/media/bg1.713d1a79.png"
+  },
+  {
+    "revision": "fddb911aa648023e40544006f29ef6d7",
+    "url": "/iot-lab/static/media/bg2.fddb911a.png"
+  },
+  {
+    "revision": "c0d577c023254fa79140bcd9c60822aa",
+    "url": "/iot-lab/static/media/blockchain.c0d577c0.png"
+  },
+  {
+    "revision": "212e8f96cea7a8823a39601273c81743",
+    "url": "/iot-lab/static/media/home2.212e8f96.png"
+  },
+  {
+    "revision": "a66b7dd57d918824b9b5abe7a8457e40",
+    "url": "/iot-lab/static/media/horse.a66b7dd5.jpg"
+  },
+  {
+    "revision": "c181f79c911aaba87515c8228ca0de6d",
+    "url": "/iot-lab/static/media/morty.c181f79c.jpg"
+  },
+  {
+    "revision": "e6f14e3f4947c17330448165aeb4c830",
+    "url": "/iot-lab/static/media/ntu.e6f14e3f.png"
+  },
+  {
+    "revision": "d768b1a62c522f9c6df02beb0129a2af",
+    "url": "/iot-lab/static/media/rick.d768b1a6.jpg"
+  },
+  {
+    "revision": "5eaddd4992e77809ae1e3ad8221a28b8",
+    "url": "/iot-lab/static/media/wjliao.5eaddd49.jpg"
+  }
+]);
