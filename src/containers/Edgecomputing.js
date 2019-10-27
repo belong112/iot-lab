f282e1677e07329093f381c8978168e4fd74ed6c
Belong112 <belongbelong223@gmail.com>
2019-10-27 12:49:28 +0800

    update_1027

--- src/containers/Edgecomputing.js
+++ src/containers/Edgecomputing.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { NavLink } from "react-router-dom"
 /*import { HashLink as Link } from 'react-router-hash-link';*/
+import pic10 from '../img/project_picture/image10.png'
 
 import ImageZoom from 'react-medium-image-zoom'
 
@@ -21,13 +22,23 @@ class Edgecomputing extends Component {
                     </div>
                     <div className="mx-3">
                         <div className="mb-5 text-justify">
-                            <div className="md-4">                                
-                                <p></p>
+                            <div className="md-4">
                                 <p>目前的研究方向如下</p>
                                 <ul>
-                                    <li>應用深度強化學習於邊緣運算之計算卸載</li>
-                                    <li>行動邊緣運算的服務佈建</li>                   
-                                </ul>                                
+                                    <li>
+                                        <h5 className='yahei'>應用深度強化學習於邊緣運算之計算卸載</h5> 
+                                    </li>    
+                                    <li>
+                                        <h5 className='yahei'>行動邊緣運算的服務佈建</h5>
+                                        <p>&emsp;&emsp;行動邊緣運算使得服務供應商可以將應用布建於網路邊緣來滿足使用者超低延遲需求，然而邊緣伺服器的運算、儲存資源不及雲端伺服器，
+                                            因此如何根據每種服務的特性、使用者的需求與邊緣伺服器的資源來進行應用服務的布建將是一個重要的研究議題。由於使用者需求會動態地變化，
+                                            在此研究中，透過深度強化學習的方式來學習一套服務布建的機制，動態地調整不同狀態下的決策，以有效地運用邊緣伺服器的資源來滿足使用者的需求。</p>
+                                        <div className="text-center">
+                                            <ImageZoom image={{src:pic10, alt: 'project_picture', className:'col-lg-7', style: { width: '100%' }}} />
+                                            <p className="font-italic"></p>
+                                        </div>
+                                    </li>
+                                </ul>                                                                 
                             </div>
                         </div>
                     </div>
